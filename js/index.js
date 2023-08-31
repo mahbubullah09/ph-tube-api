d5304398659a9f77dd401cd251ceff317d6b1452
Mahbubullah <mahbubullahpathan@gmail.com>
2023-09-01 03:22:09 +0600

    author final

--- js/index.js
+++ js/index.js
@@ -54,11 +54,11 @@ const loadVideo = async(ID) =>{
      </figure>
 
      <div class=" mt-4 flex  justify-start items-start gap-2 ml-1">
-          <figure>         
-        <img class="rounded-full min-[320px]:w-16 min-[320px]:h-9 min-[360px]:h-10 min-[360px]:w-14 min-[410px]:w-14 min-[410px]:h-12 min-[600px]:w-14 min-[600px]:h-14 md:w-16 md:h-12 lg:w-28 lg:h-12"  src=${cardData.authors[0].profile_picture} alt="">
+          <figure class="flex-initial">         
+        <img class="rounded-full w-10 h-10"  src=${cardData.authors[0].profile_picture} alt="">
      </figure>  
-        <div class="right flex-initial">
-            <h4 class="text-base font-bold ">Building a Winning UX Strategy Using the Kano Model</h4>
+        <div class="right ">
+            <h4 class="text-base font-bold ">${cardData.title}</h4>
             
             <div id="${cardData.thumbnail}-img" class="flex items-center gap-2">
             <p class="text-[#171717] text-opacity-70 ">Awlad Hossain </p>
