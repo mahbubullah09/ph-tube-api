55b5e0e2b0df6351cdb5615bdee47533552a0c20
Mahbubullah <mahbubullahpathan@gmail.com>
2023-09-01 03:58:40 +0600

    no data found

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const loadCatagory = async() =>{
+const loadCatagory = async () => {
 
-    const catagory= await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
-    const data= await catagory.json();
-    const catagoryNames= data.data;
+    const catagory = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+    const data = await catagory.json();
+    const catagoryNames = data.data;
     handleCatagory(catagoryNames)
 
 
@@ -11,41 +11,74 @@ const loadCatagory = async() =>{
 
 //handleCatagory
 
-const handleCatagory = (names) =>{
+const handleCatagory = (names) => {
     const catagoryId = document.getElementById('catagory-field');
 
-names.forEach(name => {
-    
+    names.forEach(name => {
 
-    const catagoryName= document.createElement('div')
 
-    catagoryName.innerHTML= `
+        const catagoryName = document.createElement('div')
+
+        catagoryName.innerHTML = `
     
     <button onclick="loadVideo('${name.category_id}')" class="bg-[#252525] bg-opacity-20 text-[ #252525] text-base py-1 px-3 rounded mt-2 ">${name.category}</button>`
 
-catagoryId.appendChild(catagoryName);
-});
+        catagoryId.appendChild(catagoryName);
+    });
 
 }
 
-const loadVideo = async(ID) =>{
-    console.log(ID);
+
+// load video 
+
+
+const loadVideo = async (ID) => {
+
     
+  
+
 
     const cardId = await fetch(` https://openapi.programming-hero.com/api/videos/category/${ID}`);
     const card = await cardId.json();
-   
+
     const cardDetails = card.data;
-    console.log(cardDetails);
+    console.log(cardDetails.length);
+
 
     const cardField = document.getElementById('card-field');
-    cardField.innerHTML= '';
+    cardField.innerHTML = '';
+
+    //nodata found for legnth 0 
+
+    if(cardDetails.length == 0){
+
+        cardField.innerHTML=`
+
+      
+        <div class="mx-auto col-span-4 text-center mt-6 md:mt-20 ">
+
+        <img src="./images/Icon.png" alt="">
+
+        </div>
+
+        <p class=" font-bold text-2xl md:text-5xl max-w-2xl  mx-auto  text-center col-span-4 mb-6 md:mb-20">Oops!! Sorry, There is no content here</p>
+       
+        
+        
+        
+        `
+
+
+    }
+    
+
+   
     cardDetails.forEach(cardData => {
 
         const cardInfo = document.createElement('div');
-        cardInfo.classList.add('my-2' ,'cursor-pointer');
+        cardInfo.classList.add('my-2', 'cursor-pointer');
 
-        cardInfo.innerHTML= `
+        cardInfo.innerHTML = `
         <figure class=" relative ">
         <img class="md:h-60  lg:h-44 w-full rounded" src="${cardData.thumbnail} alt="">
         <div id="${cardData.thumbnail}" class="absolute bottom-2 right-2">
@@ -73,41 +106,48 @@ const loadVideo = async(ID) =>{
         cardField.appendChild(cardInfo);
 
 
-        const vidId= document.getElementById(`${cardData.thumbnail}`);
-        console.log(vidId);
-        const postDate= cardData.others.posted_date;
-        if(postDate !== ''){
-            const postDate= cardData.others.posted_date;
-            if(postDate !== ''){
-                console.log(postDate);
-                const pera = document.createElement('p');
-                pera.classList.add('bg-black', 'text-white', 'text-xs' , 'py-1', 'px-2', 'rounded');
-                
+        // time show in video card
+
+
+        const vidId = document.getElementById(`${cardData.thumbnail}`);
+       
+        const postDate = cardData.others.posted_date;
+        if (postDate !== '') {
+            const postDate = cardData.others.posted_date;
+            if (postDate !== '') {
                
-                pera.innerHTML=`${postDate} seconds ago`;
-    
+                const pera = document.createElement('p');
+                pera.classList.add('bg-black', 'text-white', 'text-xs', 'py-1', 'px-2', 'rounded');
+
+
+                pera.innerHTML = `${postDate} seconds ago`;
+
                 vidId.appendChild(pera);
             }
         }
 
-        const verifyId= document.getElementById(`${cardData.thumbnail}-img`);
-        console.log(verifyId);
-        const verifyStatus= cardData.authors[0].verified;
-      
-        console.log(verifyStatus);     
 
-        if(verifyStatus === true){
-            console.log(verifyId);
-             const newDiv =document.createElement('div');
-             newDiv.innerHTML=`
+        //verify tick
+
+
+        const verifyId = document.getElementById(`${cardData.thumbnail}-img`);
+       
+        const verifyStatus = cardData.authors[0].verified;
+
+        
+
+        if (verifyStatus === true) {
+            
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `
              <img src="./images/fi_10629607.png" alt="">
              `
 
-             verifyId.appendChild(newDiv);
+            verifyId.appendChild(newDiv);
         }
     });
 
-    
+
 
 
 
@@ -120,6 +160,9 @@ const loadVideo = async(ID) =>{
 
 
 
-
+//load Catagory field
 loadCatagory();
+
+// inithial all catagory
+
 loadVideo('1000')
