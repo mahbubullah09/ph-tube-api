d094411d7dab19e608af9572790f6265877b43b5
Mahbubullah <mahbubullahpathan@gmail.com>
2023-09-02 03:11:11 +0600

    almost

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-const loadCatagory = async (isClicked) => {
+const loadCatagory = async (isSort) => {
 
     const catagory = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await catagory.json();
     const catagoryNames = data.data;
-    handleCatagory(catagoryNames, isClicked)
+    handleCatagory(catagoryNames, isSort)
 
 
 
@@ -11,21 +11,22 @@ const loadCatagory = async (isClicked) => {
 
 //handleCatagory
 
-const handleCatagory = (names, isClicked) => {
-    console.log(isClicked);
+const handleCatagory = (names, isSort) => {
+    console.log(isSort);
+
     const catagoryId = document.getElementById('catagory-field');
-    catagoryId.innerHTML=''
-   
+    catagoryId.innerHTML = ''
+
 
     names.forEach(name => {
 
 
         const catagoryName = document.createElement('div');
-       
-        
+
+
         catagoryName.innerHTML = `
     
-    <button onclick="loadVideo('${name.category_id}','${isClicked}')" class="bg-[#252525] bg-opacity-20 text-[ #252525] text-base py-1 px-3 rounded mt-2 ">${name.category}</button>`
+     <button onclick="loadVideo('${name.category_id}' , '${isSort}')" class="bg-[#252525] bg-opacity-20 text-[ #252525] text-base py-1 px-3 rounded mt-2 ">${name.category}</button>`
 
         catagoryId.appendChild(catagoryName);
     });
@@ -36,65 +37,64 @@ const handleCatagory = (names, isClicked) => {
 // load video 
 
 
-const loadVideo = async (ID, isClicked) => {
+const loadVideo = async (ID, isSort) => {
+    console.log(isSort);
+    console.log(ID);
     const catagoryName = document.createElement('div');
-    catagoryName.innerHTML='';
+    catagoryName.innerHTML = '';
 
- const clicked = isClicked;
- console.log(clicked);
-  
+
+
+
 
 
 
 
     const cardId = await fetch(` https://openapi.programming-hero.com/api/videos/category/${ID}`);
     const card = await cardId.json();
-    const data= card.data;
-    
-
-    let cardDetails=data;
+    const data = card.data;
 
 
-    console.log(cardDetails);
-    
+
+
+    let cardDetails = data;
+
+    const isOk = document.getElementById('click');
+    console.log(isOk);
+
+
+
+
+
+    // console.log(cardDetails);
+
+    // console.log(cardDetails[0].others.views);
+
     // cardDetails.sort((a, b) => b.others.posted_date - a.others.posted_date);
 
-    // cardDetails.sort((a, b) => {
-    //     let fa = a.others.views.toLowerCase(),
-    //         fb = b.others.views.toLowerCase();
-    
-    //     if (fa > fb) {
-    //         return -1;
-    //     }
-    //     if (fa < fb) {
-    //         return 1;
-    //     }
-    //     return 0;
-    // });
-    
+    // if(c===1){
+
+    //     cardDetails.sort((a, b) => {
+    //         let x = parseInt(a.others.views)
+    //         let y = parseInt(b.others.views)
 
 
-    cardDetails.forEach((e)=>{
-
-        console.log(`${e.others.views} ${e.others.posted_date}`);
-    })
+    //     return y-x;
+    //     });
+    // }
 
 
 
-    if(isClicked === 'true'){
-        
-        
-        console.log("object");
 
-        
-    
-    
-    }
 
- 
 
-   
-   
+
+
+
+
+
+
+
 
     // console.log(cardDetails.length);
 
@@ -125,11 +125,30 @@ const loadVideo = async (ID, isClicked) => {
 
     }
 
+    // if(isClicked === 'true'){
+
+
+    //     cardDetails.sort((a, b) => {
+    //         let x = parseInt(a.others.views)
+    //         let y = parseInt(b.others.views)
+
+
+
+    //     return y-x;
+    //     });
+
+
+
+
+    // }
+
 
 
     cardDetails.forEach(cardData => {
 
-        console.log(isClicked);
+
+
+
 
         const cardInfo = document.createElement('div');
         cardInfo.classList.add('my-2', 'cursor-pointer');
@@ -217,6 +236,140 @@ const loadVideo = async (ID, isClicked) => {
 
 
 
+    document.getElementById('click').onclick = function () {
+        const cardField = document.getElementById('card-field');
+        cardField.innerHTML = '';
+
+
+             cardDetails.sort((a, b) => {
+            let x = parseInt(a.others.views)
+            let y = parseInt(b.others.views)
+
+
+        return y-x;
+        });
+
+
+        if (cardDetails.length == 0) {
+
+            cardField.innerHTML = `
+    
+          
+            <div class="mx-auto col-span-4 text-center mt-6 md:mt-20 ">
+    
+            <img src="./images/Icon.png" alt="">
+    
+            </div>
+    
+            <p class=" font-bold text-2xl md:text-5xl max-w-2xl  mx-auto  text-center col-span-4 mb-6 md:mb-20">Oops!! Sorry, There is no content here</p>
+           
+            
+            
+            
+            `
+    
+    
+        }
+
+      
+
+
+        cardDetails.forEach(cardData => {
+
+
+
+
+
+            const cardInfo = document.createElement('div');
+            cardInfo.classList.add('my-2', 'cursor-pointer');
+
+            cardInfo.innerHTML = `
+            <figure class=" relative ">
+            <img class="md:h-60  lg:h-44 w-full rounded" src="${cardData.thumbnail} alt="">
+            <div id="${cardData.thumbnail}" class="absolute bottom-2 right-2">
+           
+             
+            </div>
+         </figure>
+    
+         <div class=" mt-4 flex  justify-start items-start gap-2 ml-1">
+              <figure class="flex-initial">         
+            <img class="rounded-full w-10 h-10"  src=${cardData.authors[0].profile_picture} alt="">
+         </figure>  
+            <div class="right ">
+                <h4 class="text-base font-bold ">${cardData.title}</h4>
+                
+                <div id="${cardData.thumbnail}-img" class="flex items-center gap-2">
+                <p class="text-[#171717] text-opacity-70 "> ${cardData.authors[0].profile_name}</p>
+                
+               
+            </div>
+                <p class="text-[#171717] text-opacity-70 ">${cardData.others.views} views</p>
+            </div>
+         </div>
+            
+            
+            `
+            cardField.appendChild(cardInfo);
+
+
+            // time show in video card
+
+
+            const vidId = document.getElementById(`${cardData.thumbnail}`);
+
+            const postDate = cardData.others.posted_date;
+
+            // convert to hours and minutes 
+
+            const hours = Math.floor(postDate / 3600);
+            const minutes = Math.floor((postDate % 3600) / 60);
+
+            const time = `${hours}hrs ${minutes} min`;
+
+            // if post date available
+
+            if (postDate !== '') {
+                const postDate = cardData.others.posted_date;
+                if (postDate !== '') {
+
+                    const pera = document.createElement('p');
+                    pera.classList.add('bg-black', 'text-white', 'text-xs', 'py-1', 'px-2', 'rounded');
+
+
+                    pera.innerHTML = `${time} ago`;
+
+                    vidId.appendChild(pera);
+                }
+            }
+
+
+            //verify tick
+
+
+            const verifyId = document.getElementById(`${cardData.thumbnail}-img`);
+
+            const verifyStatus = cardData.authors[0].verified;
+
+            //check verifyed or not? 
+
+            if (verifyStatus === true) {
+
+                const newDiv = document.createElement('div');
+                newDiv.innerHTML = `
+                 <img src="./images/fi_10629607.png" alt="">
+                 `
+
+                verifyId.appendChild(newDiv);
+            }
+        });
+
+
+    }
+
+
+
+
 
 
 
@@ -226,8 +379,10 @@ const loadVideo = async (ID, isClicked) => {
 
 }
 
-const handleSortViwes = () =>{
-    loadCatagory(true);
+const handleSortViwes = (a) => {
+
+    return a;
+
 
 
 }
